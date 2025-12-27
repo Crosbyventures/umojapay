cf7fe4eb7e2389d4bd8f9a4544c4e26f80719f37
Crosby Ventures <info.crosbyventures@gmail.com>
2025-12-26 21:18:48 -0700

    Updates

---
+++ assets/cursor-BhIPZkJP.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-Dg_n-ZM6.js";import"./index-B1X3oYxp.js";import"./index.es-C3X4kQyP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
