7216553c7da735eacfbf8e532fc0013fd004295e
Crosby Ventures <info.crosbyventures@gmail.com>
2025-12-27 16:01:34 -0700

    Updates

--- assets/chevron-right-h07b3q3w.js
+++
@@ -1,8 +0,0 @@
-import{w as o}from"./core-BJ4s0B0F.js";import"./index-DOF-xbXY.js";import"./index.es-CoZc7CxO.js";const i=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{i as chevronRightSvg};
--- assets/cursor-Dx27r3xm.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-BJ4s0B0F.js";import"./index-DOF-xbXY.js";import"./index.es-CoZc7CxO.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
